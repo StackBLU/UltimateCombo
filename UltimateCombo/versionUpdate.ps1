94a09450accd1d02866177771eef6c51dd039b96
Benjamin Wagner <174837223+StackBLU@users.noreply.github.com>
2025-09-14 10:34:16 -0600

    Update versionUpdate.ps1

--- UltimateCombo/versionUpdate.ps1
+++ UltimateCombo/versionUpdate.ps1
@@ -1,9 +1,10 @@
 [xml]$proj = Get-Content "UltimateCombo.csproj"
-$assemblyVersion = $proj.Project.PropertyGroup | Where-Object { $_.AssemblyVersion } | Select-Object -First 1 | ForEach-Object { $_.AssemblyVersion }
+$assemblyVersion = $proj.Project.PropertyGroup.AssemblyVersion | Select-Object -First 1
+
 if ($assemblyVersion) {
-    $jsonArray = Get-Content "..\pluginmaster.json" | ConvertFrom-Json
-    $jsonArray[0].AssemblyVersion = $assemblyVersion
-    "[$($jsonArray | ConvertTo-Json -Depth 10)]" | Set-Content "..\pluginmaster.json"
+    $json = Get-Content "..\pluginmaster.json" | ConvertFrom-Json
+    $json[0].AssemblyVersion = $assemblyVersion
+    "[$($json | ConvertTo-Json -Depth 10)]" | Set-Content "..\pluginmaster.json"
     Write-Host "Synced version $assemblyVersion to pluginmaster.json"
 } else {
     Write-Warning "Could not find AssemblyVersion in project file"
